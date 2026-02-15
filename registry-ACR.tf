f32c8b88b5a479b9cc72e1e24339e61ba3196f4c
vfuller1 <victor.g.fuller@gmail.com>
2026-02-15 11:06:46 -0600

    Add lifecycle ignore_changes to azurerm_role_assignment.aks_to_acr to prevent update errors

--- registry-ACR.tf
+++ registry-ACR.tf
@@ -14,4 +14,8 @@ resource "azurerm_role_assignment" "aks_to_acr" {
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
+
+  lifecycle {
+    ignore_changes = [principal_id, role_definition_name, scope]
+  }
 }
