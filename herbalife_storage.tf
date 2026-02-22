756de2354b532da7ab680955cb718a7d879d540d
vfuller1 <victor.g.fuller@gmail.com>
2026-02-21 20:06:16 -0600

    Add Lakehouse containers (raw, bronze, silver) to storage provisioning

--- herbalife_storage.tf
+++ herbalife_storage.tf
@@ -7,3 +7,22 @@ resource "azurerm_storage_account" "HL_herbalife_dev" {
   is_hns_enabled           = true
   tags                     = local.herbalife_common_tags
 }
+
+# Lakehouse containers
+resource "azurerm_storage_container" "raw" {
+  name                  = "raw"
+  storage_account_name  = azurerm_storage_account.HL_herbalife_dev.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "bronze" {
+  name                  = "bronze"
+  storage_account_name  = azurerm_storage_account.HL_herbalife_dev.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "silver" {
+  name                  = "silver"
+  storage_account_name  = azurerm_storage_account.HL_herbalife_dev.name
+  container_access_type = "private"
+}
