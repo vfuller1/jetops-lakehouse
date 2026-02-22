e02a6730676371769cbbc6b7b378129db91acf10
vfuller1 <victor.g.fuller@gmail.com>
2026-02-22 12:21:39 -0600

    Align Herbalife Event Hub resources to prod naming

--- herbalife_eventhub.tf
+++ herbalife_eventhub.tf
@@ -1,15 +1,27 @@
-resource "azurerm_eventhub_namespace" "HL_herbalife_dev" {
-  name                = "eh-herbalife-dev"
-  location            = azurerm_resource_group.HL_herbalife_dev_core.location
-  resource_group_name = azurerm_resource_group.HL_herbalife_dev_core.name
+resource "azurerm_eventhub_namespace" "herbalife_ns" {
+  name                = "herbalife-clickstream-ns-prod"
+  location            = "eastus2"
+  resource_group_name = "rg-herbalife-terraform-state-prod"
   sku                 = "Standard"
-  tags                = local.herbalife_common_tags
+  capacity            = 1
 }
 
-resource "azurerm_eventhub" "HL_herbalife_dev" {
-  name                = "eventhub-herbalife-dev"
-  namespace_name      = azurerm_eventhub_namespace.HL_herbalife_dev.name
-  resource_group_name = azurerm_resource_group.HL_herbalife_dev_core.name
+resource "azurerm_eventhub" "herbalife_hub" {
+  name                = "herbalife-clickstream-hub-prod"
+  namespace_name      = azurerm_eventhub_namespace.herbalife_ns.name
+  resource_group_name = azurerm_eventhub_namespace.herbalife_ns.resource_group_name
   partition_count     = 2
   message_retention   = 1
+}
+
+resource "azurerm_eventhub_authorization_rule" "herbalife_send" {
+  name                = "send"
+  namespace_name      = azurerm_eventhub_namespace.herbalife_ns.name
+  eventhub_name       = azurerm_eventhub.herbalife_hub.name
+  resource_group_name = azurerm_eventhub_namespace.herbalife_ns.resource_group_name
+  rights              = ["Send"]
+}
+
+output "herbalife_eventhub_send_connection_string" {
+  value = azurerm_eventhub_authorization_rule.herbalife_send.primary_connection_string
 }
