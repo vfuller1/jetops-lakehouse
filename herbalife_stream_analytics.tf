4d0b7f01c81b83c9375be8dfe410ebf0ad95ff11
vfuller1 <victor.g.fuller@gmail.com>
2026-02-23 15:41:07 -0600

    Terraform: Add Azure Function App, storage, and plan wiring for Event Hub integration

---
+++ herbalife_stream_analytics.tf
@@ -0,0 +1,68 @@
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_storage_account" "function_storage" {
+  name                     = "funcstor${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.HL_herbalife_dev_core.name
+  location                 = azurerm_resource_group.HL_herbalife_dev_core.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "function_plan" {
+  name                = "herbalife-func-plan"
+  location            = azurerm_resource_group.HL_herbalife_dev_core.location
+  resource_group_name = azurerm_resource_group.HL_herbalife_dev_core.name
+  kind                = "FunctionApp"
+  reserved            = true
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "eventhub_func" {
+  name                       = "herbalife-eventhub-func"
+  location                   = azurerm_resource_group.HL_herbalife_dev_core.location
+  resource_group_name        = azurerm_resource_group.HL_herbalife_dev_core.name
+  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
+  storage_account_name       = azurerm_storage_account.function_storage.name
+  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+  version                    = "~4"
+  os_type                    = "linux"
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME      = "python"
+    EVENTHUB_CONNECTION_STRING    = azurerm_eventhub_authorization_rule.herbalife_send.primary_connection_string
+    EVENTHUB_NAME                 = azurerm_eventhub.herbalife_hub.name
+    AzureWebJobsStorage           = azurerm_storage_account.function_storage.primary_connection_string
+  }
+}
+
+output "function_app_name" {
+  value = azurerm_function_app.eventhub_func.name
+}
+
+output "function_app_id" {
+  value = azurerm_function_app.eventhub_func.id
+}
+resource "azurerm_stream_analytics_job" "herbalife_stream_job" {
+  name                = "herbalife-stream-job"
+  location            = azurerm_resource_group.HL_herbalife_dev_core.location
+  resource_group_name = azurerm_resource_group.HL_herbalife_dev_core.name
+  streaming_units     = 1
+  compatibility_level = "1.2"
+  data_locale         = "en-US"
+  output_error_policy = "Stop"
+}
+
+output "herbalife_stream_job_name" {
+  value = azurerm_stream_analytics_job.herbalife_stream_job.name
+}
+
+output "herbalife_stream_job_id" {
+  value = azurerm_stream_analytics_job.herbalife_stream_job.id
+}
