1d29dbbb1484c5c3a6fc1fd3bfdc731904606272
vfuller1 <victor.g.fuller@gmail.com>
2026-02-24 15:54:13 -0600

    Chore: Update Databricks cluster config after troubleshooting NO_ISOLATION error

--- herbalife_databricks_cluster.tf
+++ herbalife_databricks_cluster.tf
@@ -3,10 +3,20 @@ resource "databricks_cluster" "herbalife_single_node" {
   spark_version           = "16.4.x-scala2.12"
   node_type_id            = "Standard_D8s_v3"
   autotermination_minutes = 30
-  num_workers             = 1 # Standard multi-node cluster
   
-  # Optionally, you can add tags or other settings here
-  # custom_tags = {
-  #   environment = "dev"
-  # }
+  # For a true Single Node cluster, set workers to 0
+  num_workers = 0 
+  
+  # This setting resolves the Isolation error
+  data_security_mode = "SINGLE_USER"
+
+  spark_conf = {
+    # Required for Single Node clusters
+    "spark.databricks.cluster.profile" : "singleNode"
+    "spark.master" : "local[*]"
+  }
+
+  custom_tags = {
+    "ResourceClass" = "SingleNode"
+  }
 }
