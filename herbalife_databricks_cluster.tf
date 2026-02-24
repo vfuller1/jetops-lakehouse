85a1338253ca45c60db03e71b0c5a0a88702479a
vfuller1 <victor.g.fuller@gmail.com>
2026-02-24 16:36:28 -0600

    Update Databricks cluster config to match working UI settings (single node, legacy mode, PHOTON)

--- herbalife_databricks_cluster.tf
+++ herbalife_databricks_cluster.tf
@@ -2,21 +2,18 @@ resource "databricks_cluster" "herbalife_single_node" {
   cluster_name            = "herbalife-single-node"
   spark_version           = "16.4.x-scala2.12"
   node_type_id            = "Standard_D8s_v3"
-  autotermination_minutes = 30
-  
-  # For a true Single Node cluster, set workers to 0
-  num_workers = 0 
-  
-  # This setting resolves the Isolation error
-  data_security_mode = "SINGLE_USER"
-
-  spark_conf = {
-    # Required for Single Node clusters
-    "spark.databricks.cluster.profile" : "singleNode"
-    "spark.master" : "local[*]"
-  }
-
-  custom_tags = {
-    "ResourceClass" = "SingleNode"
-  }
+    autotermination_minutes = 30
+    num_workers             = 0
+    data_security_mode      = "LEGACY_SINGLE_USER_STANDARD"
+    driver_node_type_id     = "Standard_D8s_v3"
+    runtime_engine          = "PHOTON"
+    spark_conf = {
+      "spark.master" = "local[*, 4]"
+      "spark.databricks.cluster.profile" = "singleNode"
+    }
+    custom_tags = {
+      ResourceClass = "SingleNode"
+    }
+    enable_elastic_disk = true
+    enable_local_disk_encryption = false
 }
