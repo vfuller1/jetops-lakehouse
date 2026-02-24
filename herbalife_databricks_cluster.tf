3d5acac2f2e72203c30417b2fa9faa83529c35de
vfuller1 <victor.g.fuller@gmail.com>
2026-02-24 12:11:13 -0600

    Fix: use correct photon argument for Databricks cluster

--- herbalife_databricks_cluster.tf
+++ herbalife_databricks_cluster.tf
@@ -3,7 +3,7 @@ resource "databricks_cluster" "herbalife_single_node" {
   spark_version           = "16.4.x-scala2.12"
   node_type_id            = "Standard_D8s_v3"
   autotermination_minutes = 30
-  enable_photon           = true
+  photon                  = true
   num_workers             = 0 # Single node cluster
   
   # Optionally, you can add tags or other settings here
