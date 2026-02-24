c09fa553ce9eead862d9c73f464dca9f782ef352
vfuller1 <victor.g.fuller@gmail.com>
2026-02-24 12:03:08 -0600

    Add Terraform resource for Databricks cluster configuration

---
+++ herbalife_databricks_cluster.tf
@@ -0,0 +1,13 @@
+resource "databricks_cluster" "herbalife_single_node" {
+  cluster_name            = "herbalife-single-node"
+  spark_version           = "16.4.x-scala2.12"
+  node_type_id            = "Standard_D8s_v3"
+  autotermination_minutes = 30
+  enable_photon           = true
+  num_workers             = 0 # Single node cluster
+  
+  # Optionally, you can add tags or other settings here
+  # custom_tags = {
+  #   environment = "dev"
+  # }
+}
