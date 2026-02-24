6ff3df88dc02289df39cf592762d1da0d702b520
vfuller1 <victor.g.fuller@gmail.com>
2026-02-24 15:43:42 -0600

    Fix: Set num_workers=1 for Databricks cluster to avoid NO_ISOLATION error

--- herbalife_databricks_cluster.tf
+++ herbalife_databricks_cluster.tf
@@ -3,7 +3,7 @@ resource "databricks_cluster" "herbalife_single_node" {
   spark_version           = "16.4.x-scala2.12"
   node_type_id            = "Standard_D8s_v3"
   autotermination_minutes = 30
-  num_workers             = 0 # Single node cluster
+  num_workers             = 1 # Standard multi-node cluster
   
   # Optionally, you can add tags or other settings here
   # custom_tags = {
