c45ff1192203e537d9263c232631c23ffe0ca59d
vfuller1 <victor.g.fuller@gmail.com>
2026-04-04 18:57:45 -0500

    feat: provision databricks notebook and job

--- databricks_cluster/main.tf
+++ databricks_cluster/main.tf
@@ -22,6 +22,10 @@ provider "databricks" {
 
 data "databricks_current_user" "me" {}
 
+resource "databricks_directory" "herbalife_workspace" {
+  path = "/Shared/Herbalife"
+}
+
 resource "databricks_cluster" "herbalife_single_node" {
   cluster_name                 = "herbalife-single-node"
   spark_version                = "16.4.x-scala2.12"
@@ -37,6 +41,33 @@ resource "databricks_cluster" "herbalife_single_node" {
   enable_local_disk_encryption = false
 }
 
+resource "databricks_notebook" "configure_sas_access" {
+  source = "${path.module}/../app/configure_sas_access.ipynb"
+  path   = "${databricks_directory.herbalife_workspace.path}/configure_sas_access"
+}
+
+resource "databricks_job" "configure_sas_access" {
+  name = "Herbalife Configure SAS Access"
+
+  task {
+    task_key            = "configure-sas-access"
+    existing_cluster_id = databricks_cluster.herbalife_single_node.id
+
+    notebook_task {
+      notebook_path = databricks_notebook.configure_sas_access.path
+      source        = "WORKSPACE"
+    }
+  }
+}
+
 output "herbalife_databricks_cluster_id" {
   value = databricks_cluster.herbalife_single_node.id
+}
+
+output "herbalife_databricks_notebook_path" {
+  value = databricks_notebook.configure_sas_access.path
+}
+
+output "herbalife_databricks_job_url" {
+  value = databricks_job.configure_sas_access.url
 }
